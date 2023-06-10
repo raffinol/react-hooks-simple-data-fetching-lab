2b35fb764c3aa98190d9f4e24ae7364512c44deb
Rafael Finol <rafaelfinol88@gmail.com>
2023-06-10 16:50:29 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((r) => r.json())
+      .then((data) => setImage(data.message));
+  }, []);
+
+  if (!image) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={image} alt="A Random Dog" />;
+}
+
+export default App;
